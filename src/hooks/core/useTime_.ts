ba8bc4bc0f2a993c1cccda3353a22e41e6b0c06e
zhangsai <1195073382@qq.com>
2024-03-27 09:35:55 +0800

    框架更改

---
+++ src/hooks/core/useTime_.ts
@@ -0,0 +1,16 @@
+import { tryOnUnmounted } from '@vueuse/core';
+
+//避免页面销毁，定时器还在执行
+export function useTimeFn(id, page, key) {
+  if (window[page] == undefined) {
+    window[page] = {};
+  }
+  window[page][key] = id;
+  tryOnUnmounted(() => {
+    for (const key in window[page]) {
+      clearTimeout(window[page][key]);
+      clearInterval(window[page][key]);
+    }
+  });
+  return id;
+}
