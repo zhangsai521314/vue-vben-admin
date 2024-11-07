5fe5f6fdef8539d94cd3515400d3ce001f6b4dfb
zhangsai <1195073382@qq.com>
2024-11-07 14:05:32 +0800

    测试优化

--- src/utils/MyCommon/AntVueCommon.ts
+++ src/utils/MyCommon/AntVueCommon.ts
@@ -2,14 +2,14 @@ const AntVueCommon = {
   //antvue select搜索,
   filterOption(inputValue, option, title = 'label', value = 'value') {
     return option[title]
-      ? option[title].toLowerCase().indexOf(inputValue) != -1
-      : option[value].toLowerCase().indexOf(inputValue) != -1;
+      ? option[title].toLowerCase().indexOf(inputValue.toLowerCase()) != -1
+      : option[value].toLowerCase().indexOf(inputValue.toLowerCase()) != -1;
   },
   //antvue tree-select搜索
   filterTreeNode(inputValue, treeNode, title = 'title', value = 'value') {
     return treeNode[title]
-      ? treeNode[title].toLowerCase().indexOf(inputValue) != -1
-      : treeNode[value].toLowerCase().indexOf(inputValue) != -1;
+      ? treeNode[title].toLowerCase().indexOf(inputValue.toLowerCase()) != -1
+      : treeNode[value].toLowerCase().indexOf(inputValue.toLowerCase()) != -1;
   },
 };
 export default AntVueCommon;
