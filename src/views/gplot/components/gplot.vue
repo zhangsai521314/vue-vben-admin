213868679be0ce25b94eee547b4091fec228c4bb
DESKTOP-PEL7QQ6\zs <1195073382@qq.com>
2025-09-10 17:09:12 +0800

    优化

--- src/views/gplot/components/gplot.vue
+++ src/views/gplot/components/gplot.vue
@@ -928,13 +928,13 @@
           await graphOb.draw();
           noAgileStateChangeStatus_timeId = setTimeout(() => {
             noAgileStateChangeStatus();
-          }, 3000);
+          }, 5000);
         })
         .catch((e) => {
           console.error(e);
           noAgileStateChangeStatus_timeId = setTimeout(() => {
             noAgileStateChangeStatus();
-          }, 3000);
+          }, 5000);
         });
     }
   }
